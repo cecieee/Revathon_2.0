6905dbf31645546a992276d006d458beee6f4128
Alwin Saji <alwinsaji4.cgnr@gmail.com>
2025-12-24 14:45:02 +0530

    about

--- src/sections/Highlights/About.jsx
+++ src/sections/Highlights/About.jsx
@@ -12,7 +12,7 @@ function About() {
           />
         </div>
         <div className="w-[80vw] md:w-1/2 text-center md:text-left" >
-          <h2 className="text-2xl md:text-4xl font-bold mb-6 uppercase tracking-wider"><span className='text-xl'>About</span><br></br><span className='text-[#3abfbc]'>Rev</span>-<span>A</span>-<span className='text-[#ff7046]'>Thon</span>1.0</h2>
+          <h2 className="text-xl md:text-4xl font-bold mb-6 uppercase tracking-wider"><span className='text-xl'>About</span><br></br><span className='text-[#3abfbc]'>Rev</span>-<span>A</span>-<span className='text-[#ff7046]'>Thon </span>1.0</h2>
           <p className="text-base md:text-lg leading-relaxed font-sans text-gray-300"  style={{fontFamily: "Inter, sans-serif"}}>
             India's First Reverse Engineering Hackathon is a unique 24-hour competition that
             goes beyond typical hackathons by focusing on the art of reverse engineering.
